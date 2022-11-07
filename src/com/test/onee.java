393455388204247cd6b282a3daf5fe14fc95b658
rajeshkhanna0310 <rrajeshkhanvel@gmail.com>
2022-11-07 21:04:24 +0530

    hello

---
+++ src/com/test/onee.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class onee {
+	public void method1() {
+		System.out.println("1");
+
+	}
+	public void method2() {
+		System.out.println("1");
+		
+	}
+	public void method3() {
+		System.out.println("1");
+		
+	}
+
+}
